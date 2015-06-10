4619e0eaedea021b5aa338b9b83cafe0ae858422
Timur Meyster <timur.meyster@gmail.com>
2015-06-10 01:18:48 -0700

    added geolocation identifier to the WeatherController based on current location of the user

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
-'use strict';
+// 'use strict';
 
 (function() {
 
-    var app = angular.module('AccuWeather', ['ngRoute']);
+    var app = angular.module('accuWeather', ['ngRoute']);
 
     app.config(function($routeProvider) {
         $routeProvider
             .when('/', {
                 controller: 'WeatherController',
-                templateUrl: 'app/view1/weather.html'
+                templateUrl: '/app/view1/weather.html'
             })
             .otherwise( { redirectTo: '/' } );
     });
--- app/view1/weatherController.js
+++ app/view1/weatherController.js
@@ -2,11 +2,29 @@
 
 (function() {
     var WeatherController = function($scope) {
+      $scope.location = null;
 
+      function init() {
+                window.onload = function() {
+                  var geoSuccess = function(position) {
+                    console.log(position);
+                    var lat = position.coords.latitude;
+                    var lon = position.coords.longitude;
+                    $scope.location = [lat, lon].join(",");
+                    console.log($scope.location);
+                  };
+                  var geoError = function(position) {
+                    alert('Error occurred. Error code: ' + error.code);
+                  };
+                  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+                };
+        };
+
+        init();
     };
 
-    WeatherController.$inject = ['scope'];
+    WeatherController.$inject = ['$scope'];
 
-    angular.module('AccuWeather')
+    angular.module('accuWeather')
       .controller('WeatherController', WeatherController);
 }());
